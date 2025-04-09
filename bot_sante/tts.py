26a7b87e2a708897ad9abc45f161ad67ee7740c7
Boubacar Diallo <dialloboubacar.1999@gmail.com>
2025-04-09 10:05:16 +0000

    models mis à jour

--- bot_sante/tts.py
+++ bot_sante/tts.py
@@ -5,17 +5,17 @@ from transformers import AutoTokenizer
 import uuid
 import os
 
-# Charger le modèle une seule fois
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = ParlerTTSForConditionalGeneration.from_pretrained("CONCREE/Adia_TTS").to(device)
 tokenizer = AutoTokenizer.from_pretrained("CONCREE/Adia_TTS")
 
 def generate_speech(text: str) -> str:
-    """
-    Génère un fichier audio à partir d'un texte en utilisant ParlerTTS.
-    Retourne le chemin vers le fichier .wav généré.
-    """
     try:
+        #voix pro
+        #description = "A professional, clear and composed voice, perfect for formal presentations"
+        #voix pour éducation
+        #description = "A clear and educational voice, with a flow adapted to learning"
+         #voix naturelle
         description = "A clear and educational voice, with a flow adapted to learning"
         input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
         prompt_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)
@@ -25,7 +25,6 @@ def generate_speech(text: str) -> str:
             prompt_input_ids=prompt_ids,
         )
 
-        # Nom de fichier unique pour éviter les conflits
         output_file = f"output_{uuid.uuid4().hex}.wav"
         sf.write(output_file, audio.cpu().numpy().squeeze(), model.config.sampling_rate)
 
