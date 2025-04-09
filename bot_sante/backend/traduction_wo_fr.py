e20dba8a551191c14b2fc8a191e2874bdde14a70
Boubacar Diallo <dialloboubacar.1999@gmail.com>
2025-04-09 11:59:19 +0000

    repo mis à jour

--- bot_sante/backend/traduction_wo_fr.py
+++ bot_sante/backend/traduction_wo_fr.py
@@ -2,11 +2,9 @@ from dotenv import load_dotenv
 import os
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Charger les variables d'environnement
 load_dotenv()
 token = os.getenv("HG_TOKEN")
 
-# Charger le tokenizer et le modèle de traduction
 tokenizer = AutoTokenizer.from_pretrained("AYI-TEKK/m2m100_wo_fr_1", token=token)
 model = AutoModelForSeq2SeqLM.from_pretrained("AYI-TEKK/m2m100_wo_fr_1", token=token)
 
